f7f4e30d54951f51d1002724a08389da11eafdcd
HanPil Park <hanpilpark@wmp-2013071901.local>
2017-05-04 16:46:27 +0900

    dajskdjaks

--- server.js
+++ server.js
@@ -114,22 +114,323 @@ app.post('/edit/:id', function (req, res) {
   });
 });
 
+var data1 = {
+  "data": {
+    "photoTotalCount": 20,
+    "pointSaveInformation": {
+      "guide": "<i></i>포토후기 등록 시 <em>100P</em> 적립, 일반후기 등록 시 <em>50P</em> 적립!"
+    },
+    "reviewList": {
+      "list": [
+        {
+          "content": "데이터 1입니다.",
+          "image": {
+            "attachSeq": 0,
+            "height": 0,
+            "reviewAttachfile": {
+              "attachDir": "string",
+              "attachHeight": 0,
+              "attachName": "string",
+              "attachSeq": 0,
+              "attachSize": 0,
+              "attachWidth": 0,
+              "delYn": "string",
+              "modifyDate": "string",
+              "regDate": "string",
+              "thumbMobileName": "string",
+              "thumbMobilePixelCd": 0,
+              "thumbMobileSize": 0,
+              "thumbPcName": "string",
+              "thumbPcPixelCd": 0,
+              "thumbPcSize": 0
+            },
+            "thumbPixelCd": 0,
+            "url": "http://img.wemep.co.kr/wmp-review/22/170301/61/61a71cce91b8c0706667144223ccdce4c11472d5.jpg",
+            "width": 0
+          },
+          "isFavorited": true,
+          "isMine": true,
+          "isReported": true,
+          "orderInfo": {
+            "optNo": 0,
+            "optTitle": "string",
+            "orderNo": 0,
+            "paymentId": 0,
+            "prodNm": "string",
+            "prodNo": 0,
+            "totalAmount": 0
+          },
+          "regDate": "string",
+          "reviewLikeCount": 0,
+          "reviewSeq": 0,
+          "thumbImage": {
+            "attachSeq": 0,
+            "width": 180,
+            "height": 180,
+            "reviewAttachfile": {
+              "attachDir": "string",
+              "attachHeight": 0,
+              "attachName": "string",
+              "attachSeq": 2204570,
+              "attachSize": 0,
+              "attachWidth": 0,
+              "delYn": "string",
+              "modifyDate": "string",
+              "regDate": "string",
+              "thumbMobileName": "string",
+              "thumbMobilePixelCd": 0,
+              "thumbMobileSize": 0,
+              "thumbPcName": "string",
+              "thumbPcPixelCd": 0,
+              "thumbPcSize": 0
+            },
+            "thumbPixelCd": 0,
+            "url": "http://img.wemep.co.kr/wmp-review/12/170418/a8/a80a90279c2ae00621751848375e83140b4d653a_pc.jpg",
+          },
+          "totalSatisfaction": 0,
+          "totalSatisfactionPoint": 0,
+          "userId": "qaqaqa******"
+        }
+      ],
+      "paging": {
+        "blockEnd": 1,
+        "blockStart": 10,
+        "limit": 10,
+        "page": 1,
+        "totalCount": 10,
+        "totalPage": 1
+      }
+    },
+    "satisfactionInfo": {
+      "satisfaction": 0,
+      "total": 0
+    },
+    "usePointSave": true,
+    "useSnsShare": true
+  }
+}
 
-// app.post('/topic/:id/edit', function (req, res) {
-//   var title = req.body.title;
-//   var description = req.body.description;
-//   var author = req.body.author;
-//   var id = req.params.id;
-//   var sql = 'UPDATE topic SET title=?, description=?, author=? WHERE id=?';
-//   connection.query(sql, [title, description, author, id], function (err, rows) {
-//     if (err) {
-//       res.status(500).send('Internal Server Error');
-//       console.log(err);
-//     } else {
-//       res.redirect('/topic/' + id);
-//     }
-//   });
-// });
+var data1_sort = {
+  "data": {
+    "photoTotalCount": 20,
+    "pointSaveInformation": {
+      "guide": "<i></i>포토후기 등록 시 <em>100P</em> 적립, 일반후기 등록 시 <em>50P</em> 적립!"
+    },
+    "reviewList": {
+      "list": [
+        {
+          "content": "라라라라라라라라라라라",
+          "image": {
+            "attachSeq": 0,
+            "height": 0,
+            "reviewAttachfile": {
+              "attachDir": "string",
+              "attachHeight": 0,
+              "attachName": "string",
+              "attachSeq": 0,
+              "attachSize": 0,
+              "attachWidth": 0,
+              "delYn": "string",
+              "modifyDate": "string",
+              "regDate": "string",
+              "thumbMobileName": "string",
+              "thumbMobilePixelCd": 0,
+              "thumbMobileSize": 0,
+              "thumbPcName": "string",
+              "thumbPcPixelCd": 0,
+              "thumbPcSize": 0
+            },
+            "thumbPixelCd": 0,
+            "url": "http://img.wemep.co.kr/wmp-review/22/170301/61/61a71cce91b8c0706667144223ccdce4c11472d5.jpg",
+            "width": 0
+          },
+          "isFavorited": true,
+          "isMine": true,
+          "isReported": true,
+          "orderInfo": {
+            "optNo": 0,
+            "optTitle": "string22222",
+            "orderNo": 0,
+            "paymentId": 0,
+            "prodNm": "string",
+            "prodNo": 0,
+            "totalAmount": 0
+          },
+          "regDate": "string",
+          "reviewLikeCount": 0,
+          "reviewSeq": 0,
+          "thumbImage": {
+            "attachSeq": 0,
+            "width": 180,
+            "height": 180,
+            "reviewAttachfile": {
+              "attachDir": "string",
+              "attachHeight": 0,
+              "attachName": "string",
+              "attachSeq": 2204570,
+              "attachSize": 0,
+              "attachWidth": 0,
+              "delYn": "string",
+              "modifyDate": "string",
+              "regDate": "string",
+              "thumbMobileName": "string",
+              "thumbMobilePixelCd": 0,
+              "thumbMobileSize": 0,
+              "thumbPcName": "string",
+              "thumbPcPixelCd": 0,
+              "thumbPcSize": 0
+            },
+            "thumbPixelCd": 0,
+            "url": "http://img.wemep.co.kr/wmp-review/12/170418/a8/a80a90279c2ae00621751848375e83140b4d653a_pc.jpg",
+          },
+          "totalSatisfaction": 0,
+          "totalSatisfactionPoint": 0,
+          "userId": "qaqaqa******"
+        }
+      ],
+      "paging": {
+        "blockEnd": 1,
+        "blockStart": 10,
+        "limit": 10,
+        "page": 1,
+        "totalCount": 10,
+        "totalPage": 1
+      }
+    },
+    "satisfactionInfo": {
+      "satisfaction": 0,
+      "total": 0
+    },
+    "usePointSave": true,
+    "useSnsShare": true
+  }
+}
+
+var data_type = {
+  "data": {
+    "photoTotalCount": 0,
+    "pointSaveInformation": {
+      "guide": "<i></i>포토후기 등록 시 <em>100P</em> 적립, 일반후기 등록 시 <em>50P</em> 적립!"
+    },
+    "reviewList": {
+      "list": [
+        {
+          "content": "배송 빠르고 맛있네요",
+          "image": null,
+          "isFavorited": false,
+          "isMine": false,
+          "isReported": false,
+          "orderInfo": {
+            "optNo": 0,
+            "optTitle": "싱싱한 씨없는 네이블 오렌지 9kg",
+            "orderNo": 1888886,
+            "paymentId": 0,
+            "prodNm": "01_네이블 오렌지 9kg(55과 전,후 중소과)",
+            "prodNo": 0,
+            "totalAmount": 0
+          },
+          "regDate": "2017.04.13 16:53",
+          "reviewLikeCount": 0,
+          "reviewSeq": 7486124,
+          "thumbImage": null,
+          "totalSatisfaction": 5,
+          "totalSatisfactionPoint": 0,
+          "userId": "qaqaqa******"
+        },
+        {
+          "content": "배송 빠르고 맛있네요",
+          "image": null,
+          "isFavorited": false,
+          "isMine": false,
+          "isReported": false,
+          "orderInfo": {
+            "optNo": 0,
+            "optTitle": "싱싱한 씨없는 네이블 오렌지 9kg",
+            "orderNo": 1888886,
+            "paymentId": 0,
+            "prodNm": "01_네이블 오렌지 9kg(55과 전,후 중소과)",
+            "prodNo": 0,
+            "totalAmount": 0
+          },
+          "regDate": "2017.04.13 16:53",
+          "reviewLikeCount": 0,
+          "reviewSeq": 7486124,
+          "thumbImage": null,
+          "totalSatisfaction": 5,
+          "totalSatisfactionPoint": 0,
+          "userId": "qaqaqa******"
+        }
+      ],
+      "paging": {
+        "blockEnd": 1,
+        "blockStart": 1,
+        "limit": 10,
+        "page": 1,
+        "totalCount": 2,
+        "totalPage": 1
+      }
+    },
+    "satisfactionInfo": {
+      "satisfaction": 0,
+      "total": 0
+    },
+    "usePointSave": true,
+    "useSnsShare": true
+  }
+}
+
+
+var noData = {
+  "data": {
+    "photoTotalCount": 0,
+    "pointSaveInformation": {
+      "guide": "<i></i>포토후기 등록 시 <em>100P</em> 적립, 일반후기 등록 시 <em>50P</em> 적립!"
+    },
+    "reviewList": {
+      "list": [],
+      "paging": {
+        "blockEnd": 0,
+        "blockStart": 1,
+        "limit": 10,
+        "page": 1,
+        "totalCount": 0,
+        "totalPage": 0
+      }
+    },
+    "satisfactionInfo": {
+      "satisfaction": 5,
+      "total": 0
+    },
+    "usePointSave": true,
+    "useSnsShare": true
+  }
+}
+
+
+app.get('/deal/:id/list', function(req, res, next) {
+  var page = req.query.page;
+  var sortcd = req.query.sortCd;
+  var output = req.query.outputType;
+  console.log('page', page);
+  console.log('sortcd', sortcd);
+  console.log('output', output);
+
+  switch (req.params.id) {
+    case '1':
+      if (sortcd == 2) {
+        res.json(data1_sort);
+      } else {
+        if (output == 2) {
+          res.json(noData);
+        } else {
+          res.json(data_type);
+        }
+      }
+    break;
+    default:
+      res.send('에러');
+  }
+});
 
 
 
